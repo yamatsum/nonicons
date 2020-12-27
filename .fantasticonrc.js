7745fe69794df22226c8256d9519d497e3f7bb2f
yamatsum <sy12250@gmail.com>
2020-12-28 00:17:02 +0900

    fix: codepoint.

--- .fantasticonrc.js
+++ .fantasticonrc.js
@@ -2,7 +2,7 @@ module.exports = {
   inputDir: './dist/icons',
   outputDir: './dist',
 	fontTypes: ['ttf'],
-  pathOptions: {
-    json: './src/template/icon-codepoints.json'
+	pathOptions: {
+    json: './src/template/nonicon.json'
   }
 };
