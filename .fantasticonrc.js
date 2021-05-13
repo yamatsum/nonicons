a31009bf8e7475e0e53f649245a89a9fcfcf9ef4
yamatsum <sy12250@gmail.com>
2021-05-13 20:53:05 +0900

    fix: nonicon to nonicons.

--- .fantasticonrc.js
+++ .fantasticonrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   inputDir: "./dist/icons",
   outputDir: "./dist",
-  fontTypes: ["ttf"],
+  fontTypes: ["ttf", "woff"],
   pathOptions: {
     json: "./src/template/nonicon.json",
   },
